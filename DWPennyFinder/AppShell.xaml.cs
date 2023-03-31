485f445d0225d108c638ba561ce28aa3db09d36a
jruss <jruss71@gmail.com>
2023-03-30 21:02:53 -0400

    Initial Checkin

---
+++ DWPennyFinder/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DWPennyFinder.ViewModels;
+using DWPennyFinder.Views;
+using Xamarin.Forms;
+
+namespace DWPennyFinder
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
+
---
+++ DWPennyFinder/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DWPennyFinder.Models;
+using DWPennyFinder.ViewModels;
+
+namespace DWPennyFinder.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
